911954eb0809d99f0d737a279e6cd3fcbda2f925
Camilo Jimenez <camilojj14@gmail.com>
2022-07-05 18:42:15 -0500

    Initial commit

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-
 const end = () => {
-    let rating = document.getElementsByName("rating");
-    let rate;
-    for(i = 0; i < rating.length; i++) {
-        if(rating[i].checked) {
-            rate = i + 1;
-        }
+  const $rating = document.getElementsByName("rating");
+  let rate;
+  for (i = 0; i < $rating.length; i++) {
+    if ($rating[i].checked) {
+      rate = i + 1;
     }
-    let result = document.getElementById("page2");
-    let selection = document.getElementById("selection");
+  }
+  let result = document.getElementById("page2");
+  let selection = document.getElementById("selection");
 
-    selection.innerHTML= rate;
-    result.style.display= 'flex';
-}
+  selection.innerHTML = rate;
+  result.style.display = "flex";
+};
